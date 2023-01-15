f8daa12fcf9a3c0fb3cd5620f53339f6e038391e
PatrickUnicorn <107567757+PatrickUnicorn@users.noreply.github.com>
2023-01-16 00:19:35 +0100

    amélioration composant qcm

--- TM-Informatique/src/components/Qcm.vue
+++ TM-Informatique/src/components/Qcm.vue
@@ -1,66 +1,115 @@
 <script setup>
 import { ref, computed } from 'vue'
+
 const questions = ref([
   {
-	question: "Dans quel pays se trouve-t'on ?",
-	answer: 0,
-	options: [
-		'Suisse',
-		'France',
-		'Allemagne'
-	],
-	selected: null
+    question: "Dans quel pays se trouve-t'on ?",
+    answer: 0,
+    options: [
+      'Suisse',
+      'France',
+      'Allemagne'
+    ],
+    selected: null
   },
   {
-	question: "Quel pays comporte le plus d'habitants",
-	answer: 0,
-	options: [
-		'Inde',
-		'USA',
-		'Chine'
-	],
-	selected: null
+    question: "Quel pays comporte le plus d'habitants",
+    answer: 0,
+    options: [
+      'Inde',
+      'USA',
+      'Chine'
+    ],
+    selected: null
   },
   {
-	question: "Quel est le plus grand pays au monde ?",
-	answer: 1,
-	options: [
-		'USA',
-		'Russie',
-		'Canada'
-	],
-	selected: null
+    question: "Quel est le plus grand pays au monde ?",
+    answer: 1,
+    options: [
+      'USA',
+      'Russie',
+      'Canada'
+    ],
+    selected: null
   }
 ])
+
 const quizCompleted = ref(false)
 const currentQuestion = ref(0)
+const timer = ref(60)
+
 const score = computed(() => {
-	let value = 0
-	questions.value.map(q => {
-		if (q.selected != null && q.answer == q.selected) {
-			console.log('correct');
-			value++
-		}
-	})
-	return value
+  let value = 0
+  questions.value.map(q => {
+    if (q.selected != null && q.answer == q.selected) {
+      value++
+    }
+  })
+  return value
 })
+
 const getCurrentQuestion = computed(() => {
-	let question = questions.value[currentQuestion.value]
-	question.index = currentQuestion.value
-	return question
+  let question = questions.value[currentQuestion.value]
+  question.index = currentQuestion.value
+  return question
 })
+
 const SetAnswer = (e) => {
-	questions.value[currentQuestion.value].selected = e.target.value
-	e.target.value = null
+  questions.value[currentQuestion.value].selected = e.target.value
+  e.target.value = null
 }
+
+const startTimer = () => {
+    setInterval(() => {
+        timer.value--
+        if (timer.value === 0) {
+            quizCompleted.value = true
+        }
+    }, 1000)
+}
+
 const NextQuestion = () => {
-	if (currentQuestion.value < questions.value.length - 1) {
-		currentQuestion.value++
-		return
-	}
-	
-	quizCompleted.value = true
+  if(currentQuestion.value === 0) {
+    startTimer()
+  }
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++
+    saveProgress()
+    return
+  }
+  quizCompleted.value = true
 }
+
+const resetQuiz = () => {
+  currentQuestion.value = 0
+  timer.value = 60
+  quizCompleted.value = false
+  questions.value = questions.value.map(question => {
+    question.selected = null
+    return question
+  })
+  localStorage.removeItem('quizProgress')
+}
+
+const saveProgress = () => {
+  localStorage.setItem('quizProgress', JSON.stringify({
+    currentQuestion: currentQuestion.value,
+    timer: timer.value,
+    questions: questions.value
+  }))
+}
+
+const loadProgress = () => {
+  const quizProgress = JSON.parse(localStorage.getItem('quizProgress'))
+  if (quizProgress) {
+    currentQuestion.value = quizProgress.currentQuestion
+    timer.value = quizProgress.timer
+    questions.value = quizProgress.questions
+    startTimer()
+  }
+}
+
+loadProgress()
 </script>
 
 <template>
